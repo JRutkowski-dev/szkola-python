bba801a3edbf17d5749cf4c4c59c790116896765
JRutkowski-dev <96615160+JRutkowski-dev@users.noreply.github.com>
2025-12-18 13:40:49 +0100

    Update app.py
    "Dodano choinkę"

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ def powitanie(imie):
 	return f"witaj {imie} w systemie Linux!"
 
 print (powitanie("uczniu"))
+
+def rysuj_choinke(n):
+	print(f"\n--- WESOŁYCH ŚWIĄT (Z Windowsa) ---")
+	for i in range(n):
+		print(' ' * (n - i - 1) + '*' * (2 * i + 1))
+	print(' ' * (n - 1) + '|')
+	
+if __name__ == "__main__":
+	rysuj_choinke(10)
+	
